7569d18281da53fa36e2d9bc1c7363fcf11ab70a
cvmelgarejo <cvmelgarejo@hotmail.com>
2023-01-16 08:50:32 -0300

    add view of homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: deprecated_member_use
 import 'package:flutter/material.dart';
-import 'package:personal_expenses_app/presentation/widgets/user_transaction.dart';
+import 'package:personal_expenses_app/presentation/views/home_page_view.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,30 +14,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
-            ),
-            UserTransactions()
-          ],
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/presentation/views/home_page_view.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+
+import '../widgets/user_transaction.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART!'),
+                elevation: 5,
+              ),
+            ),
+            UserTransactions()
+          ],
+        ),
+      ),
+    );
+  }
+}
